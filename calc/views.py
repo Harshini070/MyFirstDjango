0be0b3f5c6327e0cc8701ecc17f22210bab36316
Harshini070 <sree07harshini@gmail.com>
2024-07-26 10:26:31 +0530

    Initial commit

--- calc/views.py
+++ calc/views.py
@@ -4,3 +4,9 @@
 from django.shortcuts import render
 def home(request):
     return render(request,'home.html',{'name':'Harshini'})
+
+def add(request):
+    val1 = int(request.POST['num1'])
+    val2 = int(request.POST['num2'])
+    val3 = val1 + val2
+    return render(request,'results.html',{'result':val3})
